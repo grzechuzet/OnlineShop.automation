52d7fc7a335f368925f02f1f6dd873c07a3034e1
GrzechuZet <grzegorz.zawalnicki@gmail.com>
2017-11-21 19:20:15 +0100

    Repo Created

---
+++ OnlineShop.Automation/Pages/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace OnlineShop.Automation.Pages
+{
+    class MainPage
+    {
+        private IWebDriver driver;
+        private string baseURL = @"http://automationpractice.com";
+
+        public MainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void gotoMainPage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+    }
+}
---
+++ OnlineShop.Automation/SampleTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OnlineShop.Automation.Helpers;
+using OnlineShop.Automation.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace OnlineShop.Automation
+{
+    [TestFixture]
+    class SampleTest
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            MainPage home = new MainPage(driver);
+            home.gotoMainPage();
+
+            Assert.AreEqual(ConstantsHelper.indexURL,driver.Url.ToString());
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
